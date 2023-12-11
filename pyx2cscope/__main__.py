ff5809e8acef5e6f281655e56f456afa2bfbeef2
Yash Agarwal <yash.agarwal@microchip.com>
2023-12-11 10:43:17 +0100

    added logging level change directly from CMD

--- pyx2cscope/__main__.py
+++ pyx2cscope/__main__.py
@@ -1,23 +1,25 @@
-import sys
-
-from PyQt5.QtWidgets import QApplication
-
-from pyx2cscope.gui.minimal_gui import X2Cscope_GUI
-
 import logging
+import sys
+from PyQt5.QtWidgets import QApplication
+from gui.watchView.minimal_gui import X2Cscope_GUI
+
 # Define a function to set the logging level based on a string argument
 def set_logging_level(level_str):
     numeric_level = getattr(logging, level_str.upper(), None)
-    if not isinstance(numeric_level, int):
-        raise ValueError(f"Invalid log level: {level_str}")
-    print(numeric_level)
-    logging.basicConfig(level=numeric_level)
-
+    if numeric_level is None:
+        logging.info(f"Invalid logging level: {level_str}")
+    else:
+        logging.root.setLevel(numeric_level)
+        logging.info(f"Logging level set to {numeric_level}")
+        logging.debug("Debug message")
+        logging.info("Info message")
+        logging.warning("Warning message")
+        logging.error("Error message")
 
 if len(sys.argv) > 1:
     log_level_arg = sys.argv[1]
-    set_logging_level(log_level_arg)
 
+set_logging_level(log_level_arg)
 app = QApplication(sys.argv)
 ex = X2Cscope_GUI()
 ex.show()
