14d500518f01729af57b0f80fbb2847b6e8ffb70
Edras Pacola <edras.pacola@microchip.com>
2024-05-02 11:04:10 +0200

    enhancing test messages

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -26,12 +26,14 @@ class TestParser:
         fake_serial(mocker, 16)
         x2c_scope = X2CScope(port="COM14", elf_file=self.elf_file_16)
         variable = x2c_scope.get_variable("motor.estimator.zsmt.iqHistory")
-        assert variable.is_array() == True
-        assert len(variable) == 4
+        assert variable is not None, "variable name not found"
+        assert variable.is_array() == True, "variable should be an array"
+        assert len(variable) == 4, "array has wrong length"
 
     def test_array_variable_32(self, mocker):
         fake_serial(mocker, 32)
         x2c_scope = X2CScope(port="COM14", elf_file=self.elf_file_32)
-        variable = x2c_scope.get_variable("motor.estimator.zsmt.iqHistory")
-        assert variable.is_array() == True
-        assert len(variable) == 4
+        variable = x2c_scope.get_variable("mcFocI_ModuleData_gds.dOutput.duty")
+        assert variable is not None, "variable name not found"
+        assert variable.is_array() == True, "variable should be an array"
+        assert len(variable) == 3, "array has wrong length"
