dc07dd51bc5a3c57b4f09e436f536cae924e36f7
Edras Pacola <edras.pacola@microchip.com>
2024-05-02 13:01:00 +0200

    fixing SerialStub.py with real data

--- tests/test_install.py
+++ tests/test_install.py
@@ -2,32 +2,33 @@ import os
 
 from mchplnet.services.frame_device_info import DeviceInfo
 from mchplnet.services.frame_load_parameter import LoadScopeData
+from pyx2cscope.xc2scope import X2CScope
 from tests import data
 from tests.utils.SerialStub import fake_serial
 
-from pyx2cscope.xc2scope import X2CScope
+elf_file_16 = os.path.join(os.path.dirname(data.__file__), "mc_foc_sl_fip_dspic33ck_mclv48v300w.elf")
 
 
-elf_file_16 = os.path.join(
-    os.path.dirname(data.__file__), "mc_foc_sl_fip_dspic33ck_mclv48v300w.elf"
-)
-
-
-elf_file_32 = os.path.join(
-    os.path.dirname(data.__file__), "qspin_foc_same54.elf"
-)
+elf_file_32 = os.path.join(os.path.dirname(data.__file__), "qspin_foc_same54.elf")
 
 
 def test_x2cscope_install_serial_16(mocker):
     fake_serial(mocker, 16)
     pyx2cscope = X2CScope(elf_file=elf_file_16, port="COM11")
-    # TODO: check if device_info and scope data are correct
     device_info: DeviceInfo = pyx2cscope.lnet.device_info
+    assert device_info.uc_width == 2, "wrong processor bit length"
+    assert device_info.processor_id == "__GENERIC_MICROCHIP_DSPIC__", "unknown processor"
     scope_data: LoadScopeData = pyx2cscope.lnet.scope_data
+    assert scope_data.scope_state == 0, "wrong scope state value"
+    assert scope_data.data_array_size == 15000, "wrong scope data size"
+
 
 def test_x2cscope_install_serial_32(mocker):
     fake_serial(mocker, 32)
     pyx2cscope = X2CScope(elf_file=elf_file_32, port="COM11")
-    # TODO: check if device_info and scope data are correct
     device_info: DeviceInfo = pyx2cscope.lnet.device_info
+    assert device_info.uc_width == 4, "wrong processor bit length"
+    assert device_info.processor_id == "__GENERIC_MICROCHIP_PIC32__", "unknown processor"
     scope_data: LoadScopeData = pyx2cscope.lnet.scope_data
+    assert scope_data.scope_state == 0, "wrong scope state value"
+    assert scope_data.data_array_size == 8192, "wrong scope array size"
--- tests/utils/SerialStub.py
+++ tests/utils/SerialStub.py
@@ -23,13 +23,12 @@ class SerialStub:
         # Start of frame, Data frame size
         if self.data == self.get_device_info:
             return bytearray(
-                b"\x55\x2E\x01\x00\x00\x04\x00\x00\x00\xFF\x01\x51\x41\x70\x72\x31\x38\x32\x30\x31\x32\x31\x31"
-                b"\x35\x35\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x00\x00\x00\x00\x00\x00\x00\x00"
-                b"\x00\x00\x00\x96"
+                b"U.\x01\x00\x00\x05\x00\x01\x00\xff\x10\x82Nov2320231500\x00\x00\x00\x00\x00\x00\xccJ"
+                b"\x14K\xc0\xff\xff\x01\x00\x00\x00\x00\x00\x00\xf4R\x00\x00\xba"
             )
         elif self.data == self.load_param:
             return bytearray(
-                b"\x55\x05\x01\x07\x00\x00\x60\x0F\xD1"
+                b"U\x1f\x01\x11\x00\x00\x04\x00\x00\x98:\x00\x004\x10\x00\x00\xe0\x05\x00\x00\x00\x00\x00\x00\x98:\x00\x00\x98:\x00\x00\x82\xab"
             )
         assert False, "Unexpected message received"
 
@@ -37,13 +36,14 @@ class SerialStub:
         # Start of frame, Data frame size
         if self.data == self.get_device_info:
             return bytearray(
-                b"\x55\x2E\x01\x00\x00\x04\x00\x00\x00\xFF\x01\x51\x41\x70\x72\x31\x38\x32\x30\x31\x32\x31\x31"
-                b"\x35\x35\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x00\x00\x00\x00\x00\x00\x00\x00"
-                b"\x00\x00\x00\x96"
+                b"U.\x01\x00\x00\x05\x00\x01\x00\xff \x82Mar 320191220Mar 320191220\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00 T"
             )
         elif self.data == self.load_param:
-            return bytearray(b"\x55\x05\x01\x07\x00\x00\x60\x0F\xD1")
+            return bytearray(
+                b"U\x1f\x01\x11\x00\x00\x00\x00\x00\x00\x00\x00\x00\xa8\x00\x00 \x00\x00\x00\x00\x00\x00\x00\x00\x00 \x00\x00\x00 \x00\x00\x82\x10"
+            )
         assert False, "Unexpected message received"
+
 
 def fake_serial(mocker, uc_width=16):
     serial_stub = SerialStub()
@@ -51,9 +51,7 @@ def fake_serial(mocker, uc_width=16):
     mocker.patch.object(LNetSerial, "write", serial_stub.LNetSerial_write)
     if uc_width == 16:
         mocker.patch.object(LNetSerial, "read", serial_stub.LNetSerial_read16)
-        mocker.patch.object(FrameDeviceInfo, "_get_processor_id", return_value=2)
     elif uc_width == 32:
         mocker.patch.object(LNetSerial, "read", serial_stub.LNetSerial_read32)
-        mocker.patch.object(FrameDeviceInfo, "_get_processor_id", return_value=4)
     else:
         raise ValueError("Mocker fake_serial: wrong uC width! should be either 16 or 32 bits")
