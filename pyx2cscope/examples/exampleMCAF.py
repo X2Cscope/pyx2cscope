b580267a5e814dfd9b62fa1f8408149c1e39b918
Edras Pacola <edras.pacola@microchip.com>
2025-11-19 13:29:58 +0100

    fixing example readability

--- pyx2cscope/examples/exampleMCAF.py
+++ pyx2cscope/examples/exampleMCAF.py
@@ -8,22 +8,18 @@ from pyx2cscope.x2cscope import X2CScope
 
 # Configuration for serial port communication
 serial_port = get_com_port()  # Get the COM port from the utility function
-baud_rate = 115200  # Set baud rate for serial communication
-
 # Specify the path to the ELF file
 elf_file = get_elf_file_path()  # Get the path to the ELF file from the utility function
 
 # Initialize the X2CScope with the specified serial port and baud rate
 # The default baud rate is 115200
-x2c_scope = X2CScope(port=serial_port, baud_rate=baud_rate, elf_file=elf_file)
+x2c_scope = X2CScope(port=serial_port, elf_file=elf_file)
 
 # Retrieve specific variables from the MCU
-speed_reference = x2c_scope.get_variable(
-    "motor.apiData.velocityReference"
-)  # Get the speed reference variable
-speed_measured = x2c_scope.get_variable(
-    "motor.apiData.velocityMeasured"
-)  # Get the measured speed variable
+# Get the speed reference variable
+speed_reference = x2c_scope.get_variable("motor.apiData.velocityReference")
+# Get the measured speed variable
+speed_measured = x2c_scope.get_variable("motor.apiData.velocityMeasured")
 
 # Attempt to read the value of the 'speedReference' variable and log the result
 try:
