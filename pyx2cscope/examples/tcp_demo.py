9d2749b97a328f32d06bfedbb46e1c04b23e6e37
Edras Pacola <edras.pacola@microchip.com>
2026-02-04 15:04:37 +0100

    oficial tcp demo

--- pyx2cscope/examples/tcp_demo.py
+++ pyx2cscope/examples/tcp_demo.py
@@ -1,35 +1,24 @@
-"""Demo scripting for user to get started."""
+"""Demo scripting for user to get started with TCP-IP."""
 import time
 
-from mchplnet.interfaces.factory import InterfaceType
 from pyx2cscope.x2cscope import X2CScope
 
-from pyx2cscope.utils import get_elf_file_path, get_host_address
+elf_file = r"path to your elf file.elf"
+host_address = r"IP address of your device"
+# The device should have a TCP server enabled listening at port 12666
 
-x2cscope = X2CScope(host=get_host_address(), interface=InterfaceType.TCP_IP, elf_file=get_elf_file_path())
+x2cscope = X2CScope(host=host_address, elf_file=elf_file)
 
-counter = x2cscope.get_variable("my_counter")
-rx_len = x2cscope.get_variable("tcp_rx_count")
-rx_full = x2cscope.get_variable("tcp_rx_full")
+phase_current = x2cscope.get_variable("motor.iabc.a")
+phase_voltage = x2cscope.get_variable("motor.vabc.a")
 
-x2cscope.add_scope_channel(counter)
-x2cscope.set_sample_time(1)
+x2cscope.add_scope_channel(phase_current)
+x2cscope.add_scope_channel(phase_voltage)
 
 x2cscope.request_scope_data()
 
 while True:
-    start = time.perf_counter()
-    ready_status =  x2cscope.is_scope_data_ready()
-    end = time.perf_counter()
-    elapsed = end - start  # in seconds
-    print(f"Time taken to check: {elapsed:.6f} seconds")
-    if ready_status:
-        start = time.perf_counter()
-        data = x2cscope.get_scope_channel_data()
-        end = time.perf_counter()
-        elapsed = end - start  # in seconds
-        print(f"Time taken to get data: {elapsed:.6f} seconds")
-        print(data)
-        print("Biggest RX:", rx_len.get_value(), " rx full?", rx_full.get_value())
+    if x2cscope.is_scope_data_ready():
+        print(x2cscope.get_scope_channel_data())
         x2cscope.request_scope_data()
     time.sleep(0.1)
