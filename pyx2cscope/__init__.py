d0bac8992a45167f22d515c1480594a415c4e818
Edras Pacola <edras.pacola@microchip.com>
2026-03-06 14:23:15 +0100

    v0.6.1

--- pyx2cscope/__init__.py
+++ pyx2cscope/__init__.py
@@ -1,11 +1,11 @@
 """This module contains the pyx2cscope package.
 
-Version: 0.6.0
+Version: 0.6.1
 """
 
 import logging
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 def set_logger(
     level: int = logging.ERROR,
